4873de1250a154ada59deb719abf3679fe53b2ea
Joseph <josephappenteng99@gmail.com>
2023-11-28 10:03:55 +0100

    m6prog_project/.env_example

--- weer.sql
+++ weer.sql
@@ -1,10 +1,15 @@
-CREATE TABLE IF NOT EXISTS weersomstandighedenPerDag (
+CREATE SCHEMA  weer;
+USE weer;
+
+CREATE TABLE IF NOT EXISTS weersomstandighedenPerDag 
+(
     idweersomstandighedenPerDag INT UNSIGNED NOT NULL,
     Datum DATE NOT NULL,
     aantalGraden DECIMAL NOT NULL,
-    plaats VARCHAR NOT NULL,
+    plaats VARCHAR(120) NOT NULL,
     windKracht INT NOT NULL,
     regenInMilimeters DECIMAL NOT NULL,
+    
     PRIMARY KEY (idweersomstandighedenPerDag),
     UNIQUE INDEX idweersomstandighedenPerDag_UNIQUE (idWeersomstandighedenPerDag ASC) VISIBLE
 );
